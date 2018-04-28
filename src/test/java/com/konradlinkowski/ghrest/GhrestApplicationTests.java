9e0c1fba846727d6d86fd07c4389eff9d85c3ba4
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-04-28 13:21:04 +0200

    Initial commit

---
+++ src/test/java/com/konradlinkowski/ghrest/GhrestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.konradlinkowski.ghrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GhrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
