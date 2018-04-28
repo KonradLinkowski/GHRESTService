ddb2d55ff0a36445a115e3c825677961be34de08
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-04-28 17:28:48 +0200

    Added pathparams to RepoController getRepo method.

--- src/main/java/com/konradlinkowski/ghrest/controller/RepoController.java
+++ src/main/java/com/konradlinkowski/ghrest/controller/RepoController.java
@@ -6,7 +6,9 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.konradlinkowski.ghrest.model.Repo;
@@ -17,9 +19,11 @@ public class RepoController {
 	@Autowired
 	private RepoService repoService;
 	
-	@RequestMapping("/test")
-	public Repo getUsers() throws InterruptedException, ExecutionException {
-		CompletableFuture<Repo> repo = repoService.getRepo("KonradLinkowski", "Scricher");
+	@RequestMapping(value="/repositories/{owner}/{repository-name}", method = RequestMethod.GET)
+	public Repo getUsers(@PathVariable("owner") String userName,
+			@PathVariable("repository-name") String repoName)
+					throws InterruptedException, ExecutionException {
+		CompletableFuture<Repo> repo = repoService.getRepo(userName, repoName);
         return repo.get();
 	}
 }
